daa5d27be6601e80dc6c44f858c3a878d9d0572a
ri003074 <ri003074@gmail.com>
2021-01-02 14:50:53 +0900

    add

---
+++ src/array.ts
@@ -0,0 +1,17 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+
+let numbers2: Array<number> = [1,2,3]
+
+let strings: string[] = ['type', 'script']
+
+
+let nijigenhairetsu:number[][] = [
+    [1,2],
+    [3,4]
+];
+
+
+
+let hairetsu: (string | number | boolean)[] = [1, false, 'Japan']
