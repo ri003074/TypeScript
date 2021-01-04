655b801e1e08f5b607167c579c71ab9c248622c1
ri003074 <ri003074@gmail.com>
2021-01-04 00:44:09 +0000

    add

--- src/unknown.ts
+++ src/unknown.ts
@@ -1,5 +1,16 @@
-export {};
+export { };
 
 const kansu = (): number => 43;
+
+const numberAny: any = kansu();
+const numberUnknown: unknown = kansu();
+
+let sumAny = numberAny + 10;
+console.log(typeof numberUnknown)
+
+// type gard 型ガード
+if (typeof numberUnknown === 'number') {
+    let sumUnknown = numberUnknown + 10;
+}
 
 
